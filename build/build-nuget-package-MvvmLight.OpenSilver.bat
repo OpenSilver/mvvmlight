8a4032ec53ce63953befcb7b93f2b67325d7661e
Pierre Barjon <barjon.pierre@gmail.com>
2023-10-13 17:53:12 +0200

    Update opensilver version

--- build/build-nuget-package-MvvmLight.OpenSilver.bat
+++ build/build-nuget-package-MvvmLight.OpenSilver.bat
@@ -12,7 +12,7 @@ rem Define the escape character for colored text
 for /F %%a in ('"prompt $E$S & echo on & for %%b in (1) do rem"') do set "ESC=%%a"
 
 rem Define the "%PackageVersion%" variable:
-set /p PackageVersion="%ESC%[92mMvvmLight.OpenSilver version:%ESC%[0m 1.0.0-private-"
+set /p PackageVersion="%ESC%[92mMvvmLight.OpenSilver version:%ESC%[0m "
 
 rem Get the current date and time:
 for /F "tokens=2" %%i in ('date /t') do set currentdate=%%i
@@ -20,7 +20,7 @@ set currenttime=%time%
 
 rem Create a Version.txt file with the date:
 md temp
-@echo MvvmLight.OpenSilver 1.0.0-private-%PackageVersion% (%currentdate% %currenttime%)> temp/Version.txt
+@echo MvvmLight.OpenSilver %PackageVersion% (%currentdate% %currenttime%)> temp/Version.txt
 
 echo. 
 echo %ESC%[95mRestoring NuGet packages%ESC%[0m
@@ -34,7 +34,7 @@ msbuild slnf/MvvmLight.OpenSilver.slnf -p:Configuration=Release -clp:ErrorsOnly 
 echo. 
 echo %ESC%[95mPacking %ESC%[0mMvvmLight.OpenSilver %ESC%[95mNuGet package%ESC%[0m
 echo. 
-nuget.exe pack nuspec\MvvmLight.OpenSilver.nuspec -OutputDirectory "output/MvvmLight.OpenSilver" -Properties "PackageId=MvvmLight.OpenSilver;PackageVersion=1.0.0-private-%PackageVersion%;Configuration=Release;Target=MvvmLight.OpenSilver;RepositoryUrl=https://github.com/OpenSilver/OpenSilver"
+nuget.exe pack nuspec\MvvmLight.OpenSilver.nuspec -OutputDirectory "output/MvvmLight.OpenSilver" -Properties "PackageId=OpenSilver.MvvmLight;PackageVersion=%PackageVersion%;Configuration=Release"
 
 explorer "output\MvvmLight.OpenSilver"
 
