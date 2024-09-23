55c03310085ea3bbf81de2377095a915c851954f
Pierre Barjon <barjon.pierre@gmail.com>
2024-09-24 00:07:19 +0200

    Update build

--- build/Build_NuGet.bat
+++ build/Build_NuGet.bat
@@ -1,5 +1,15 @@
 @ECHO off
 
+SETLOCAL
+
+IF "%~1" == "--help" (
+	GOTO :help
+)
+
+IF "%~1" == "-h" (
+	GOTO :help
+)
+
 SET BUILD_DIR=%~dp0
 SET SRC_DIR=%~dp0..\GalaSoft.MvvmLight
 SET CFG=Release
@@ -13,7 +23,7 @@ IF "%~1" == "" (
 	SET /P OpenSilverPkgVersion="%ESC%[92mOpenSilver version:%ESC%[0m "
 ) ELSE (
 	SET PackageVersion=%1
-	if "%~2" == "" (
+	IF "%~2" == "" (
 		SET OpenSilverPkgVersion=%1
 	) ELSE (
 		SET OpenSilverPkgVersion=%2
@@ -23,9 +33,17 @@ IF "%~1" == "" (
 ECHO. 
 ECHO %ESC%[95mBuilding %ESC%[0m%CFG% %ESC%[95mconfiguration%ESC%[0m
 ECHO. 
-msbuild %SRC_DIR%\GalaSoft.MvvmLight.OpenSilver.sln -p:Configuration=%CFG% -clp:ErrorsOnly -restore
+msbuild %SRC_DIR%\GalaSoft.MvvmLight.OpenSilver.sln -p:Configuration=%CFG%;OpenSilverVersion=%OpenSilverPkgVersion% -verbosity:minimal -restore
 
 ECHO. 
 ECHO %ESC%[95mPacking %ESC%[0mOpenSilver.MvvmLight %ESC%[95mNuGet package%ESC%[0m
 ECHO. 
 %BUILD_DIR%\nuget.exe pack %BUILD_DIR%\nuspec\OpenSilver.MvvmLight.nuspec -OutputDirectory "%BUILD_DIR%\output" -Properties "PackageVersion=%PackageVersion%;OpenSilverDependencyVersion=%OpenSilverPkgVersion%;Configuration=%CFG%"
+
+EXIT /b
+
+:help
+ECHO [1] OpenSilver.MvvmLight NuGet package Version
+ECHO [2] OpenSilver Version
+
+ENDLOCAL
